c95685f47c27c83371ab49016780fa530b2c4dc7
khalil hadji <khalil.hadji@dataimpact.io>
2024-05-29 12:50:23 +0200

    fix added htmlquery package

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/H-ADJI/ace
 
 go 1.21.5
+
+require github.com/antchfx/htmlquery v1.3.1
+
+require (
+	github.com/antchfx/xpath v1.3.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/antchfx/htmlquery"
+)
 
 func main() {
+	resp, err := http.Get("https://www.techiedelight.com/data-structures-and-algorithms-problems/")
+	if err != nil {
+		fmt.Println("something went wrong")
+		return
+	}
+	defer resp.Body.Close()
+	source, err := htmlquery.Parse(resp.Body)
+	if err != nil {
+		fmt.Println("couldnt parse resp body")
+		return
+	}
+	elmnts, err := htmlquery.Query(source, "//div")
+	if err != nil {
+		fmt.Println("couldnt query source")
 
-	fmt.Println("lol")
+	}
+	elmnts = htmlquery.FindOne(elmnts, "//h1/text()")
+	fmt.Println(elmnts.Data)
 }
