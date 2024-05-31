2ecdc54265810b68bb9e652dcf0c177e195f3681
h-adji <h-adji_tech@proton.me>
2024-05-31 09:43:15 +0200

    add scraping func

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/H-ADJI/ace
 
-go 1.21.5
+go 1.22
 
 require github.com/antchfx/htmlquery v1.3.1
 
--- main.go
+++ main.go
@@ -2,28 +2,13 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 
-	"github.com/antchfx/htmlquery"
+	ace "github.com/H-ADJI/ace/cmd"
 )
 
 func main() {
-	resp, err := http.Get("https://www.techiedelight.com/data-structures-and-algorithms-problems/")
-	if err != nil {
-		fmt.Println("something went wrong")
-		return
+	challenges := ace.GetChallenges()
+	for i := range 100 {
+		fmt.Println(challenges[i])
 	}
-	defer resp.Body.Close()
-	source, err := htmlquery.Parse(resp.Body)
-	if err != nil {
-		fmt.Println("couldnt parse resp body")
-		return
-	}
-	elmnts, err := htmlquery.Query(source, "//div")
-	if err != nil {
-		fmt.Println("couldnt query source")
-
-	}
-	elmnts = htmlquery.FindOne(elmnts, "//h1/text()")
-	fmt.Println(elmnts.Data)
 }
